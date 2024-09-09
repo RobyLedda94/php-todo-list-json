32c18f131cf56812efacf4bffbccf403d416247a
RobyLedda94 <robyledda01@yahoo.it>
2024-09-09 16:56:44 +0200

    [FIX] percorso dove specifico il file json

--- server.php
+++ server.php
@@ -3,7 +3,9 @@
     header('Content-type: application/json');
 
     // imposto la variabile in cui specifico il nome del file dove e contenuto l'array
-    $file = 'todo-list.json'
+    $file = 'data/todo-list.json';
 
-    
+    // leggo il contenuto del file json e lo restituisco
+
+    echo file_get_contents($file);
 ?>
