3566590d421dc8434c5982873bae6ef2aaf99cde
RobyLedda94 <robyledda01@yahoo.it>
2024-09-09 16:04:30 +0200

    [UPD] cdn vue, axios e scaffoldin base del file index.php e script.js

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- cdn vue -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!-- cdn axios -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js" integrity="sha512-odNmoc1XJy5x1TMVMdC7EMs3IVdItLPlCeL5vSUPN2llYKMJ2eByTTAIiiuqLg+GdNr9hF6z81p27DArRFKT7A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <title>Todo List</title>
+</head>
+<body>
+   <div id="app">
+    
+   </div> 
+<!-- importo il file javascript -->
+<script src="./js/script.js" type="text/javascript"></script>
+</body>
+</html>
