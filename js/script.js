ceba301baa0b89f2e8f7e9e1d234d3510db4e754
RobyLedda94 <robyledda01@yahoo.it>
2024-09-09 17:03:47 +0200

    [UPD] recupero la todo list

--- js/script.js
+++ js/script.js
@@ -3,10 +3,17 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            // inizializzo un array vuoto per la todo-list
+            todos: []
         }
     },
     methods: {
+        // recupero la lista todo dal server
+        getTodoList() {
+            axios.get('server.php').then(response => {
+
+            })
+        }
 
     },
     mounted() {
